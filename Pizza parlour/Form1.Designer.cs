fc097a01af151dc9842bc72182d26b053f932915
Stratford Central <engineeringcentral@outlook.com>
2020-11-26 13:59:39 -0500

    Add project files.

---
+++ Pizza parlour/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace Pizza_parlour
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Screen = new System.Windows.Forms.Label();
+            this.Toppings = new System.Windows.Forms.TextBox();
+            this.Topings = new System.Windows.Forms.Label();
+            this.Costofdough = new System.Windows.Forms.TextBox();
+            this.Pizza = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Canadian = new System.Windows.Forms.Label();
+            this.Maplesyrup = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Calculator = new System.Windows.Forms.Button();
+            this.Screen2 = new System.Windows.Forms.Label();
+            this.Change = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Changetitale = new System.Windows.Forms.Label();
+            this.Changeowed = new System.Windows.Forms.Label();
+            this.Costofpizza = new System.Windows.Forms.Label();
+            this.Costoftoppings = new System.Windows.Forms.Label();
+            this.Costofmaple = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Screen
+            // 
+            this.Screen.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Screen.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Screen.ForeColor = System.Drawing.Color.Black;
+            this.Screen.Location = new System.Drawing.Point(358, 78);
+            this.Screen.Name = "Screen";
+            this.Screen.Size = new System.Drawing.Size(442, 375);
+            this.Screen.TabIndex = 0;
+            // 
+            // Toppings
+            // 
+            this.Toppings.Location = new System.Drawing.Point(6, 169);
+            this.Toppings.Name = "Toppings";
+            this.Toppings.Size = new System.Drawing.Size(267, 20);
+            this.Toppings.TabIndex = 1;
+            // 
+            // Topings
+            // 
+            this.Topings.Font = new System.Drawing.Font("mono 07_55", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Topings.ForeColor = System.Drawing.Color.Transparent;
+            this.Topings.Location = new System.Drawing.Point(3, 113);
+            this.Topings.Name = "Topings";
+            this.Topings.Size = new System.Drawing.Size(305, 28);
+            this.Topings.TabIndex = 2;
+            this.Topings.Text = "Number of toppings";
+            // 
+            // Costofdough
+            // 
+            this.Costofdough.Location = new System.Drawing.Point(6, 101);
+            this.Costofdough.Name = "Costofdough";
+            this.Costofdough.Size = new System.Drawing.Size(267, 20);
+            this.Costofdough.TabIndex = 3;
+            // 
+            // Pizza
+            // 
+            this.Pizza.Font = new System.Drawing.Font("mono 07_55", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pizza.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Pizza.Location = new System.Drawing.Point(6, 36);
+            this.Pizza.Name = "Pizza";
+            this.Pizza.Size = new System.Drawing.Size(270, 39);
+            this.Pizza.TabIndex = 4;
+            this.Pizza.Text = "Number of pizza";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            // 
+            // Canadian
+            // 
+            this.Canadian.Font = new System.Drawing.Font("mono 07_55", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Canadian.ForeColor = System.Drawing.Color.White;
+            this.Canadian.Location = new System.Drawing.Point(172, 1);
+            this.Canadian.Name = "Canadian";
+            this.Canadian.Size = new System.Drawing.Size(430, 35);
+            this.Canadian.TabIndex = 6;
+            this.Canadian.Text = "The Canadian pizza store";
+            // 
+            // Maplesyrup
+            // 
+            this.Maplesyrup.BackColor = System.Drawing.Color.Transparent;
+            this.Maplesyrup.Font = new System.Drawing.Font("mono 07_55", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Maplesyrup.ForeColor = System.Drawing.Color.White;
+            this.Maplesyrup.Location = new System.Drawing.Point(6, 187);
+            this.Maplesyrup.Name = "Maplesyrup";
+            this.Maplesyrup.Size = new System.Drawing.Size(346, 39);
+            this.Maplesyrup.TabIndex = 7;
+            this.Maplesyrup.Text = "Bottles of maple syrup";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 243);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(346, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // Calculator
+            // 
+            this.Calculator.ForeColor = System.Drawing.Color.Black;
+            this.Calculator.Location = new System.Drawing.Point(6, 270);
+            this.Calculator.Name = "Calculator";
+            this.Calculator.Size = new System.Drawing.Size(346, 23);
+            this.Calculator.TabIndex = 9;
+            this.Calculator.Text = "Calculator";
+            this.Calculator.UseVisualStyleBackColor = true;
+            this.Calculator.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Screen2
+            // 
+            this.Screen2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Screen2.CausesValidation = false;
+            this.Screen2.ForeColor = System.Drawing.Color.Black;
+            this.Screen2.Location = new System.Drawing.Point(6, 300);
+            this.Screen2.Name = "Screen2";
+            this.Screen2.Size = new System.Drawing.Size(346, 73);
+            this.Screen2.TabIndex = 10;
+            // 
+            // Change
+            // 
+            this.Change.BackColor = System.Drawing.Color.White;
+            this.Change.ForeColor = System.Drawing.Color.Black;
+            this.Change.Location = new System.Drawing.Point(6, 402);
+            this.Change.Name = "Change";
+            this.Change.Size = new System.Drawing.Size(346, 23);
+            this.Change.TabIndex = 11;
+            this.Change.Text = "Change owed ehh";
+            this.Change.UseVisualStyleBackColor = false;
+            this.Change.Click += new System.EventHandler(this.Change_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(114, 376);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(238, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(354, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(446, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Print recept ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Changetitale
+            // 
+            this.Changetitale.BackColor = System.Drawing.Color.Brown;
+            this.Changetitale.Font = new System.Drawing.Font("mono 07_55", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Changetitale.ForeColor = System.Drawing.Color.Transparent;
+            this.Changetitale.Location = new System.Drawing.Point(12, 373);
+            this.Changetitale.Name = "Changetitale";
+            this.Changetitale.Size = new System.Drawing.Size(103, 27);
+            this.Changetitale.TabIndex = 14;
+            this.Changetitale.Text = "Change";
+            // 
+            // Changeowed
+            // 
+            this.Changeowed.BackColor = System.Drawing.Color.White;
+            this.Changeowed.Font = new System.Drawing.Font("mono 07_55", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Changeowed.ForeColor = System.Drawing.Color.White;
+            this.Changeowed.Location = new System.Drawing.Point(6, 428);
+            this.Changeowed.Name = "Changeowed";
+            this.Changeowed.Size = new System.Drawing.Size(346, 23);
+            this.Changeowed.TabIndex = 15;
+            // 
+            // Costofpizza
+            // 
+            this.Costofpizza.Font = new System.Drawing.Font("mono 07_55", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Costofpizza.ForeColor = System.Drawing.Color.Transparent;
+            this.Costofpizza.Location = new System.Drawing.Point(8, 75);
+            this.Costofpizza.Name = "Costofpizza";
+            this.Costofpizza.Size = new System.Drawing.Size(265, 23);
+            this.Costofpizza.TabIndex = 16;
+            this.Costofpizza.Text = "$5 per pizza";
+            // 
+            // Costoftoppings
+            // 
+            this.Costoftoppings.AutoSize = true;
+            this.Costoftoppings.Font = new System.Drawing.Font("mono 07_55", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Costoftoppings.ForeColor = System.Drawing.Color.White;
+            this.Costoftoppings.Location = new System.Drawing.Point(7, 141);
+            this.Costoftoppings.Name = "Costoftoppings";
+            this.Costoftoppings.Size = new System.Drawing.Size(192, 25);
+            this.Costoftoppings.TabIndex = 17;
+            this.Costoftoppings.Text = "$2 per toppings";
+            // 
+            // Costofmaple
+            // 
+            this.Costofmaple.Font = new System.Drawing.Font("mono 07_55", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Costofmaple.ForeColor = System.Drawing.Color.White;
+            this.Costofmaple.Location = new System.Drawing.Point(12, 217);
+            this.Costofmaple.Name = "Costofmaple";
+            this.Costofmaple.Size = new System.Drawing.Size(340, 23);
+            this.Costofmaple.TabIndex = 18;
+            this.Costofmaple.Text = "$50 per bottle";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Brown;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Costofmaple);
+            this.Controls.Add(this.Costoftoppings);
+            this.Controls.Add(this.Costofpizza);
+            this.Controls.Add(this.Changeowed);
+            this.Controls.Add(this.Changetitale);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Change);
+            this.Controls.Add(this.Screen2);
+            this.Controls.Add(this.Calculator);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Maplesyrup);
+            this.Controls.Add(this.Canadian);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Pizza);
+            this.Controls.Add(this.Costofdough);
+            this.Controls.Add(this.Topings);
+            this.Controls.Add(this.Toppings);
+            this.Controls.Add(this.Screen);
+            this.ForeColor = System.Drawing.Color.DarkCyan;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Screen;
+        private System.Windows.Forms.TextBox Toppings;
+        private System.Windows.Forms.Label Topings;
+        private System.Windows.Forms.TextBox Costofdough;
+        private System.Windows.Forms.Label Pizza;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Canadian;
+        private System.Windows.Forms.Label Maplesyrup;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button Calculator;
+        private System.Windows.Forms.Label Screen2;
+        private System.Windows.Forms.Button Change;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label Changetitale;
+        private System.Windows.Forms.Label Changeowed;
+        private System.Windows.Forms.Label Costofpizza;
+        private System.Windows.Forms.Label Costoftoppings;
+        private System.Windows.Forms.Label Costofmaple;
+    }
+}
+
---
+++ Pizza parlour/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace Pizza_parlour
+    //Name:David 
+    //Nov,26,2020
+    //Basis summary
+    //Basic cash register simulation 
+{
+    public partial class Form1 : Form
+    {//Global variables 
+        //For calculator
+        Double dough=5;
+        int costofdoughr;
+        Double moneyfromDough;
+       
+
+        Double toppings=2;
+        int costoftoppings;
+        Double moneyfromToppings;
+
+        Double Maple=50;
+        int costofMaple;
+        Double MoneyfromMaple;
+        //For calculator & recept 
+        Double cost;
+        //For recept 
+        Double amounttax;
+        Double tax = 1.13;
+        Double Smalltax= 0.13;
+        Double totalCost;
+        // For change 
+        Double Money;
+        Double change;
+        Double moneyowed;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //This button is for the calculator 
+        private void Button1_Click(object sender, EventArgs e)
+            
+        {
+            try
+            {
+                costofdoughr = Convert.ToInt32(Costofdough.Text);
+                moneyfromDough = dough * costofdoughr;
+
+                costoftoppings = Convert.ToInt32(Toppings.Text);
+                moneyfromToppings = toppings * costoftoppings;
+
+                costofMaple = Convert.ToInt32(textBox2.Text);
+                MoneyfromMaple = Maple * costofMaple;
+            
+        
+            
+            
+                cost = moneyfromDough + moneyfromToppings + MoneyfromMaple;
+
+                Screen2.Text = $"The cost of pizza  {moneyfromDough.ToString("c")} \nThe cost of toppings will be { moneyfromToppings.ToString("c")} \n cost of maple syrup is ehh {MoneyfromMaple.ToString("c")} \n\n total cost is {cost.ToString("c")} ";
+            }
+
+
+            catch
+            {
+                Screen2.Text = $"Sorry ehh use only numbers ehhh";
+            }
+
+
+        }
+
+        //This is the change owed change  
+        private void Change_Click(object sender, EventArgs e)
+        {
+            //change=
+        }
+
+        //Second button is print recipt 
+        private void Button2_Click(object sender, EventArgs e)
+            
+        { try
+            {
+                SoundPlayer player = new SoundPlayer(Properties.Resources.cash);
+                amounttax = cost * Smalltax;
+                totalCost = cost * tax;
+                player.Play();
+                Screen.Text = $"The Canadian pizza store";
+                Refresh();
+                Thread.Sleep(1000);
+                player.Play();
+                Screen.Text+=    $"\nPizza              {moneyfromDough.ToString("c")}";
+                Refresh();
+                Thread.Sleep(1000);
+                player.Play();
+                Screen.Text+=$"\nToopings       {moneyfromToppings.ToString("c")}";
+                  Refresh();
+                Thread.Sleep(1000);
+                player.Play();
+                Screen.Text+=$"\nMaple syrup  {MoneyfromMaple.ToString("c")} ";
+                Refresh();
+                Thread.Sleep(1000);
+                player.Play();
+                Screen.Text+=$"\n\nCost               {cost.ToString("c")} ";
+                Refresh();
+                Thread.Sleep(1000);
+                player.Play();
+                Screen.Text+=$"\nTax                 {amounttax.ToString("c")} ";
+                Refresh();
+                Thread.Sleep(1000);
+                player.Play();
+                Screen.Text+=$"\nTotal cost      {totalCost.ToString("c")} ";
+                Refresh();
+                Thread.Sleep(1000);
+                player.Play();
+                Screen.Text+=$"\n\n Have a nice day.  ";
+
+            }
+            catch
+            {
+                Screen.Text = $"only numbers ok";
+            }
+        }
+    }
+}
